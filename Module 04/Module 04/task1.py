7dabc8ba381f72a571790c73f7e6097097c90e83
Chidatma <patelchidatma@gmail.com>
2025-10-21 19:46:50 +0530

    Delete Module 04/Module 04 directory

--- Module 04/Module 04/task1.py
+++
@@ -1,9 +0,0 @@
-user_input = int(input("Enter a number: "))
-def fact(n):
-    if(n==1 or n==0): #base condition
-        return 1
-    else:
-        return n * fact(n-1)
-factorial = fact(user_input)
-print(f"Factorial of {user_input} is: {factorial}")
-
