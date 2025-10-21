fc69c01e046819c6670900a8a2a09794f09ed69a
Chidatma <patelchidatma@gmail.com>
2025-10-21 19:48:08 +0530

    Fix formatting and ensure proper imports in task2.py

--- Module 04/task2.py
+++ Module 04/task2.py
@@ -3,5 +3,3 @@ user_input = int(input("Enter a number: "))
 print(f"Square root: {math.sqrt(user_input)}")
 print(f"Logarithm: {math.log(user_input)}")
 print(f"Sine: {math.sin(user_input)}")
-
-print(math.pi)
