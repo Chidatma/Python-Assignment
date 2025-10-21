9df75ff121901ab0c463f2442c8e4ab3fc6e29f3
Chidatma <patelchidatma@gmail.com>
2025-10-21 19:44:45 +0530

    Add files via upload

---
+++ Module 04/Module 04/task2.py
@@ -0,0 +1,7 @@
+import math
+user_input = int(input("Enter a number: "))
+print(f"Square root: {math.sqrt(user_input)}")
+print(f"Logarithm: {math.log(user_input)}")
+print(f"Sine: {math.sin(user_input)}")
+
+print(math.pi)
